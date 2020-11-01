eec075bbbc12cddce46eac486ae60252a6a545d7
kaihulme <kaihulme97@gmail.com>
2020-11-01 13:48:27 +0000

    Flag params

--- 04_sobel_edge_detector_hough_transform/src/edges.cpp
+++ 04_sobel_edge_detector_hough_transform/src/edges.cpp
@@ -23,86 +23,118 @@ void NormaliseAngles(cv::Mat &input);
 void Threshold(const cv::Mat &input, const double threshold_val, 
 			   cv::Mat &thresholded_output);
 
-// TODO change to args: ./edges {file} -s(obel) -t(hreshold) {t_value} -g(aussian) {k_size}
 // TODO if -t but not -s check if sobel outputs are already in /out
 // TODO add function for Convolution(input, kernel, width, height, output)
 int main(int argc, char* argv[]) {
+	
 	// check arg count
-	if      (argc < 2) { printf("\nError: image not specified!\n\n");        return -1; }
-	else if (argc > 5) { printf("\nError: check your flags (-s -t -g)\n\n"); return -1; }
+	if      (argc < 2)  { printf("\nError: image not specified!\n\n");     return -1; }
+	else if (argc == 2) { printf("\nError: operation not specified!\n\n"); return -1; }
+
 	// get image location
 	const string img_arg = argv[1];
 	const string img_loc = "resources/" + img_arg + ".png";
+	
 	// read image data
  	const Mat image = imread(img_loc, 1);
  	if(!image.data ) {
    		printf("\nError: image not found!\n\n");
    		return -1;
  	}
+	
 	// create grey image
  	Mat img_grey;
  	cvtColor(image, img_grey, CV_BGR2GRAY);
-	// bools for flags
-	bool sobel     = false;
-	bool threshold = false;
-	bool gaussian  = false;
+	
+	// values for flag handling
+	bool sobel, threshold, gaussian = false;
+	int gaussian_val, threshold_val = 0;
+	
 	// handle flags and set bools
 	for (int i=2; i<argc; i++) {
-		if      (!strcmp(argv[i], "-s") && !sobel)     sobel     = true;
-		else if (!strcmp(argv[i], "-t") && !threshold) threshold = true; 
-		else if (!strcmp(argv[i], "-g") && !gaussian)  gaussian  = true; 
+		if      (!strcmp(argv[i], "-s") && !sobel) {
+			sobel = true;
+		}
+		else if (!strcmp(argv[i], "-t") && !threshold) {
+			try {
+				threshold_val = std::stoi(argv[i+1]);
+				threshold = true;
+				i++;
+			} catch (std::exception const &e) {
+				std::cout << "\nError: threshold value not specified\n" << std::endl; 
+				return -1;
+			}
+		} 
+		else if (!strcmp(argv[i], "-g") && !gaussian)  {
+			try {
+				gaussian_val = std::stoi(argv[i+1]);
+				gaussian = true;
+				i++;
+			} catch (std::exception const &e) {
+				std::cout << "\nError: kernel size not specified\n" << std::endl; 
+				return -1;
+			} 
+		}
 		else { 
-			std::cout << "\nError: check your flags (-s -t -g)\n" << std::endl; 
+			std::cout << "\nError: check your flags (-s -t [threshold_value] -g [kernel_size])\n" << std::endl; 
 			return -1; 
 		}
-	}	
+	}
+	
+	if (threshold && !sobel) { printf("\nError: cannot threshold magnitudes without appling edge detection (-s)!\n\n"); return -1; }
+
 	// apply gaussian filter
 	if (gaussian) { 
-		int usr_kernel_size = 0;
-        std::cout << "Gaussian filter size: ";
-		std::cin >> usr_kernel_size;
 		Mat img_input;
-		GaussianBlur(img_grey, usr_kernel_size, img_input);
+		GaussianBlur(img_grey, gaussian_val, img_input);
+		const string gaussian_out = "out/" + img_arg + "_gaussian.jpg";
+		cv::imwrite(gaussian_out, img_input);
 	}
 	else Mat img_input = img_grey;
+	
 	// sobel edge detector
     if (sobel) {
+	
 		// set kernel size and output matrices
         const int kernel_size = 3;
         Mat img_dfdx, img_dfdy;
 		Mat img_magnitude, img_direction;
+	
 		// apply sobel edge detection
 		SobelEdgeDetector(img_grey, kernel_size, 
 						  img_dfdx, img_dfdy,
 						  img_magnitude, img_direction);
+	
 		// set file names
         const string dfdx_out = "out/" + img_arg + "_dfdx.jpg";
 		const string dfdy_out = "out/" + img_arg + "_dfdy.jpg";
 		const string magnitude_out = "out/" + img_arg + "_magnitude.jpg";
 		const string direction_out = "out/" + img_arg + "_direction.jpg";
+	
 		// write images to files
-		imwrite(dfdx_out, img_dfdx);
-		imwrite(dfdy_out, img_dfdy);
-		imwrite(magnitude_out, img_magnitude);
-		imwrite(direction_out, img_direction);
+		cv::imwrite(dfdx_out, img_dfdx);
+		cv::imwrite(dfdy_out, img_dfdy);
+		cv::imwrite(magnitude_out, img_magnitude);
+		cv::imwrite(direction_out, img_direction);
+	
 		// if thresholding
 		if (threshold) {
-			// get threshold value and set threshold matrix
-			double threshold_val=0.0;
-			std::cout << "Threshold value: ";
-			std::cin >> threshold_val;
+			// threshold output image
 			Mat img_thresholded_magnitude;
 			// threshold magnitudes
 			Threshold(img_magnitude, threshold_val, 
-					img_thresholded_magnitude);
+					  img_thresholded_magnitude);
 			// set file name and write image to file
 			string threshold_direction_out = "out/" + img_arg + "_thresholded_magnitude.jpg";
-			imwrite(threshold_direction_out, img_thresholded_magnitude);
+			cv::imwrite(threshold_direction_out, img_thresholded_magnitude);
 		}
+	
 	}
+	
 	// edge detection complete
 	std::cout << "...\n\nComplete!\n" << std::endl;
- 	return 0;
+ 	
+	return 0;
 }
 
 void SobelEdgeDetector(const cv::Mat &input, const int size, 
@@ -110,11 +142,13 @@ void SobelEdgeDetector(const cv::Mat &input, const int size,
 					   cv::Mat &dfdy_output,
 					   cv::Mat &magnitude_output, 
 					   cv::Mat &direction_output) {
+	
 	// intialise the output using the input
 	dfdx_output.create(input.size(), cv::DataType<double>::type);
 	dfdy_output.create(input.size(), cv::DataType<double>::type);
 	magnitude_output.create(input.size(), cv::DataType<double>::type);
 	direction_output.create(input.size(), cv::DataType<double>::type);
+	
 	// df/fx kernel
 	double dfdx_kernel_vals[3][3] = {{-1, 0, 1},
 						   			 {-1, 0, 1},
@@ -123,23 +157,29 @@ void SobelEdgeDetector(const cv::Mat &input, const int size,
     double dfdy_kernel_vals[3][3] = {{-1,-1,-1},
 									 { 0, 0, 0},
 									 { 1, 1, 1}};
+	
 	// create opencv matrix from values
 	cv::Mat dfdx_kernel = cv::Mat(size, size, cv::DataType<double>::type, dfdx_kernel_vals);
     cv::Mat dfdy_kernel = cv::Mat(size, size, cv::DataType<double>::type, dfdy_kernel_vals);
+	
 	// set kernel radius for padding and convolution
 	const int r_x = (size-1)/2;
 	const int r_y = (size-1)/2;
+	
 	// replicate edge at border to allow edge convolutions
 	cv::Mat padded_input;
 	cv::copyMakeBorder(input, padded_input, 
 					   r_x, r_x, r_y, r_y,
 					   cv::BORDER_REPLICATE);
+	
 	// apply convolution to each pixel in image
 	for (int i=0; i<input.rows; i++) {	
 		for(int j=0; j<input.cols; j++) {
+	
 			// sum for matrix multiplication
 			double dfdx_sum = 0.0;
             double dfdy_sum = 0.0;
+	
 			// for each kernel value
 			for(int m=-r_x; m<=r_x; m++) {
 				for(int n=-r_y; n<=r_y; n++ ) {
@@ -157,19 +197,23 @@ void SobelEdgeDetector(const cv::Mat &input, const int size,
                     dfdy_sum += img_val * dfdy_kernel_val;
 				}
 			}
-            // magnitude: ∇|f(x,y)| = sqrt( (df/dx)^2 + (df/dy)^2 )
+    
+	        // magnitude: ∇|f(x,y)| = sqrt( (df/dx)^2 + (df/dy)^2 )
 			// direction: φ = arctan( (df/dy) / (df/dx) )
 			double dfdx_pixel      = dfdx_sum;
 			double dfdy_pixel      = dfdy_sum;
             double magnitude_pixel = sqrt((dfdx_sum*dfdx_sum) + (dfdy_sum*dfdy_sum));
 			double direction_pixel = atan2(dfdy_sum, dfdx_sum);
+	
 			// update the image with new pixel value
 			dfdx_output.at<double>(i, j)      = dfdx_pixel;
 			dfdy_output.at<double>(i, j)      = dfdy_pixel;
 			magnitude_output.at<double>(i, j) = magnitude_pixel;
 			direction_output.at<double>(i, j) = direction_pixel;
+	
 		}
 	}
+	
 	// normalise pixel values to range 0-255
 	NormalisePixels(dfdy_output);
 	NormalisePixels(dfdx_output);
@@ -179,26 +223,32 @@ void SobelEdgeDetector(const cv::Mat &input, const int size,
 
 void GaussianBlur(const cv::Mat &input, const int size, 
 				  cv::Mat &gaussian_output) {
+	
 	// intialise the output using the input
 	gaussian_output.create(input.size(), input.type());
+	
 	// create the gaussian kernel
 	cv::Mat kX = cv::getGaussianKernel(size, -1);
 	cv::Mat kY = cv::getGaussianKernel(size, -1);
 	cv::Mat gaussian_kernel = kX * kY.t();
+	
 	// get radius of kernel for padding and convolution
 	const int r_x = (size-1)/2;
 	const int r_y = (size-1)/2;
+	
 	// scale kernel values for size of gaussian kernel
 	for( int m=-r_x; m <=r_x; m++) {
 	  	for( int n=-r_y; n<=r_y; n++) {
 		   	gaussian_kernel.at<double>(m+r_x, n+r_y) = (double) 1.0/(size*size);
 		}
 	}
+	
 	// replicate edge at border to allow edge convolutions
 	cv::Mat padded_input;
 	cv::copyMakeBorder(input, padded_input, 
 					   r_x, r_x, r_y,r_y,
 					   cv::BORDER_REPLICATE);
+	
 	// apply convolution to each pixel in image
 	for (int i=0; i<input.rows; i++) {	
 		for(int j=0; j<input.cols; j++) {
@@ -223,13 +273,16 @@ void GaussianBlur(const cv::Mat &input, const int size,
 			gaussian_output.at<uchar>(i, j) = (uchar) gaussian_sum;
 		}
 	}
+
 }
 
 void NormalisePixels(cv::Mat &input) {
+
 	// get min and max values in input
 	double minVal, maxVal; 
 	Point minLoc, maxLoc;
 	minMaxLoc(input, &minVal, &maxVal, &minLoc, &maxLoc);
+
 	// for each pixel in input
 	for (int i=0; i<input.rows; i++) {	
 		for (int j=0; j<input.cols; j++) {
@@ -240,15 +293,18 @@ void NormalisePixels(cv::Mat &input) {
 			input.at<double>(i, j) = normalised_pixel;
 		}
 	}
+
 }
 
 void NormaliseAngles(cv::Mat &input) {
+
 	// set pi constant      
 	const double pi = atan(1)*4;
 	// get min and max values in input
 	double minVal, maxVal; 
 	Point minLoc, maxLoc;
 	minMaxLoc(input, &minVal, &maxVal, &minLoc, &maxLoc);
+
 	// for rach pixel in input
 	for (int i=0; i<input.rows; i++) {	
 		for (int j=0; j<input.cols; j++) {
@@ -263,12 +319,15 @@ void NormaliseAngles(cv::Mat &input) {
 			}
 		}
 	}
+
 }
 
 void Threshold(const cv::Mat &input, const double threshold_val, 
 			   cv::Mat &thresholded_output) {
+	
 	// intialise the output using the input
 	thresholded_output.create(input.size(), input.type());
+	
 	// for each pixel in input
 	for (int i=0; i<input.rows; i++) {	
 		for(int j=0; j<input.cols; j++) {
@@ -283,4 +342,5 @@ void Threshold(const cv::Mat &input, const double threshold_val,
 			thresholded_output.at<double>(i, j) = thresholded_pixel;
 		}
 	}
+
 }
